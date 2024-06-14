db42e6358e705a2efc7460639a9b9b140ec25dfa
~Srimal <150960173+srimal0@users.noreply.github.com>
2024-06-14 21:33:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,7 +55,7 @@ module.exports = {
   BOT_OFFLINE : false,
   INBOX_BLOCK : false,
   AUTO_REACT : false,
-  OWNER_REACT : '🥷🏻',
+  OWNER_REACT : '🎴',
   AUTO_BGM : false,
   BGM : {
     "chaio" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-caio.mp3",
